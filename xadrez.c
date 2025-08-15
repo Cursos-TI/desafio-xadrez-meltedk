c159f15180d286be78cfed01b0167bb28880b1e5
Humberto Michaelsen Bohrer <111300178+meltedk@users.noreply.github.com>
2025-08-15 18:59:08 +0000

    refatora a lógica de movimentação das peças para validar a entrada do usuário

--- xadrez.c
+++ xadrez.c
@@ -33,6 +33,8 @@ int main() {
 	int numCasasTorre, numCasasBispo, numCasasRainha;
 	int i;
 	int movimentoCompleto = 1; // Flag para controlar o movimento em 'L'
+	const int torreExcessao = 5, bispoExcessao = 5, rainhaExcessao = 8;
+	const int excessaoMin = 1;
 
 	printf("\t*** Movimentos de Xadrez ***\n\n");
 
@@ -40,19 +42,43 @@ int main() {
 	printf("\nMovimento da torre: \n");
 	printf("Digite quantas casas você quer mover a torre para direita: ");
 	scanf("%d", &numCasasTorre);
-	moverTorre(numCasasTorre);
+	if(numCasasTorre > 0 && numCasasTorre <= 5) {
+		moverTorre(numCasasTorre);
+	} else if(numCasasTorre > 5) {
+		printf("Número de casas inválido! (Máximo de 5 casas)\n");
+		moverTorre(torreExcessao);
+	} else {
+		printf("Número de casas inválido! (Mínimo de 1 casa)\n");
+		moverTorre(excessaoMin);
+	}
 
 	//Movimento do bispo
 	printf("\nMovimento do bispo: \n");
 	printf("Digite quantas casas você quer mover o bispo para diagonal superior direita: ");
 	scanf("%d", &numCasasBispo);
-	moverBispo(numCasasBispo);
+	if(numCasasBispo > 0 && numCasasBispo <= 5) {
+		moverBispo(numCasasBispo);
+	} else if(numCasasBispo > 5) {
+		printf("Número de casas inválido! (Máximo de 5 casas)\n");
+		moverBispo(bispoExcessao);
+	} else {
+		printf("Número de casas inválido! (Mínimo de 1 casa)\n");
+		moverBispo(excessaoMin);
+	}
 
 	//Movimento da rainha
 	printf("\nMovimento da rainha: \n");
 	printf("Digite quantas casas você quer mover a rainha para esquerda: ");
 	scanf("%d", &numCasasRainha);
-	moverRainha(numCasasRainha);
+	if(numCasasRainha > 0 && numCasasRainha <= 8) {
+		moverRainha(numCasasRainha);
+	} else if(numCasasRainha > 8) {
+		printf("Número de casas inválido! (Máximo de 8 casa)\n");
+		moverRainha(rainhaExcessao);
+	} else {
+		printf("Número de casas inválido! (Mínimo de 1 casa)\n");
+		moverRainha(excessaoMin);
+	}
 
 	//Movimento do cavalo
 	printf("\nMovimento do cavalo: \n");
